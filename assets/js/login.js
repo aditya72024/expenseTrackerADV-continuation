3c7e5b59acc9dcd8420e3cbfc9b475735b439368
Aditya Ojha <aditya72024@gmail.com>
2023-11-15 17:31:00 +0530

    thirdcommitwithLoginFrontendPartialBackend

---
+++ assets/js/login.js
@@ -0,0 +1,88 @@
+var loginForm = document.getElementById("loginForm");
+loginForm.addEventListener('submit',login);
+
+
+function setSuccess(e){
+    const inputControl = e.parentElement;
+    const errorDisplay = inputControl.querySelector('.fielderror');
+    errorDisplay.innerText = "";
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+function setError(e, message){
+    const inputControl = e.parentElement;
+    const errorDisplay = inputControl.querySelector('.fielderror');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function login(e){
+    e.preventDefault();
+    console.log('hi');
+    if(validateInput()){
+        
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const data = {};
+
+        
+        data.email = email;
+        data.password = password;
+
+        console.log(data);
+
+         axios.post("http://localhost:5000/user/login",{data
+         })
+         .then(res =>{
+            alert(res.data.success);
+         })
+         .catch(error=>{
+            if(error){
+                alert(error.response.data.error);
+            }
+         });
+
+    }else{
+        console.log(false)
+    }
+}
+
+function validateInput(){
+    
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    let flag = 1;
+
+
+
+    if(email.value.trim() === '') {
+        setError(email, 'Email is required');
+        flag = 0;
+    } else if (!isValidEmail(email.value.trim())) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(password.value.trim() === '') {
+        setError(password, 'Password is required');
+        flag = 0;
+    }else {
+        setSuccess(password);
+    }
+
+    if(flag == 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
--- controllers/user.js
+++ controllers/user.js
@@ -23,3 +23,33 @@ exports.signup = async (req, res, next) => {
 
     }
 }
+
+exports.login = async (req, res, next) => {
+    try{
+        if(!req.body.data.email && !req.body.data.password ){
+            throw new Error('All details are mandatory!!!');
+        }
+
+        const email = req.body.data.email;
+        const password = req.body.data.password;
+
+        const data = await User.findAll({where : {email:email}});
+
+        console.log(data);
+
+        if(data.length == 0){
+            res.status(404).json({error:"User not found!!!"});
+        }else if(password != data[0].dataValues.password){
+            res.status(404).json({error:"Password Incorrect!!!"});
+        }else{
+            res.status(201).json({success:"User Logged in!!!"})
+        }
+        
+
+        // res.status(201).json(data);
+
+    }catch(err){
+        res.status(500).json({error:err})
+
+    }
+}
