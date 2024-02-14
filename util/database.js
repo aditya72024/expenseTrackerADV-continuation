0861b709f32ed65c56593ad8830a0d2c9572d918
Aditya Ojha <aditya72024@gmail.com>
2024-02-14 10:58:16 +0530

    readyToDeploy3

--- util/database.js
+++ util/database.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './.env' });
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     dialect: 'mysql',
-    host: process.env.DB_NAME
+    host: process.env.DB_HOST
 })
 
 module.exports = sequelize;
